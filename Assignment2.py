522cfe971ff9499ccb43a195a0b5d169cdbbb6d7
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-07 09:53:01 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -218,53 +218,37 @@
 # print("Largest:", larg)
 # print("Smallest:", smal
 
-l=eval(input("Enter a list of numbers"))
+# l=eval(input("Enter a list of numbers"))
 # [4,7,9,10,45,21,46,67,23] --- input
-print("min=",min(l))
-print("max=",max(l))
-
-
-
-
-
-
-
-
-
-
-
+# print("min=",min(l))
+# print("max=",max(l))
 
 
 # 13. Write a Python program to print the numbers of a specified list after removing even numbers from it. 
 
 
-
-
-
-
-
-
-
-
-
-
+# list = [11, 22, 33, 44, 55]
+# # print("specified list:")
+# print(list)
+# for i in list:
+#     if i % 2 == 0:
+#         list.remove(i)
+# print("List after removing EVEN numbers:")
+# print(list)
 
 
 
 # 14. Write a Python program to generate and print a list of first and last 5 elements where the values are square of numbers between 1 and 30
 
 
-
-
-
-
-
-
-
-
-
-
-
+# def printValues():
+# 	l = list()
+# 	for i in range(1,30):
+# 		l.append(i**2)
+# 	print(l[:5])
+# 	print(l[-5:])
+	
+# printValues()
 
 
 
@@ -272,28 +256,166 @@ print("max=",max(l))
 # 15. Write a Python program to insert a given string at the beginning of all items in a list. 
 
 
-
-
-
-
-
-
-
-
-
+# def mylist(list, str):
+ 
+#     str += '{0}'
+#     list = [str.format(i) for i in list]
+#     return(list)
+ 
+ 
+# list = [1, 2, 3, 4]
+# str = 'apple'
+# print(mylist(list, str))
 
 
 
 
 # 16.  Write a Python program to iterate over two lists simultaneously
 
+# Iterating over single lists, refers to using for loops for iteration over a single element of a single list at a particular step whereas in iterating over multiple lists simultaneously,
+#  we refer using for loops for iteration over a single element of multiple lists at a particular step. 
+
+
+# import itertools
+ 
+# num = [1, 2, 3]
+# color = ['red', 'while', 'black']
+# value = [255, 256]
+# for (a, b, c) in zip(num, color, value):
+#      print (a, b, c)
+
+
+# 17  . Write a Python program to add a key to a dictionary.
+
+
+ # dict = {'name': 'vrinda', 'place': 'palakkad'}
+# print("keys to the dictionary:")
+ 
+# print (dict)
+
+
+#  18. Write a Python script to concatenate following dictionaries to create a new one.
+
+
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# dic4 = {}
+# for d in (dic1, dic2, dic3): dic4.update(d)
+# # print(dic4)
+
+
+# 19. Write a Python program to iterate over dictionaries using for loops.
+
+# You can loop through a dictionary by using a for loop.
+# When looping through a dictionary, the return value are the keys of the dictionary,
+# but there are methods to return the values as well.
+
+# dict_1 = {'ID':123,'NAME':'sana','PLACE':'kochi','AGE':19}
+
+# for key, value in dict_1.items():
+#     print(key, value)
 
 
 
+#  20.  Write a Python program to sum all the items in a dictionary.
 
 
+# def Sum(myDict):
+ 
+#     list = []
+#     for i in myDict:
+#         list.append(myDict[i])
+#     final = sum(list)
+ #     return final
+ 
+# dict = {'a': 100, 'b': 200, 'c': 300}
+# print("Sum :", Sum(dict))
 
 
+# 21.   Create a dictionary to hold information about pets. Each key is an animal's name, and each value is the kind of animal.Eg : {'Dog':'Willie'}
+
+    # Put at least 3 key-value pairs in your dictionary.
+    # Use a for loop to print out a series of statements such as "Willie is a dog."
+
+# 1. Create a dictionary named pets to hold information about pets. Each key is
+      # an animal’s name, and each value is the type of animal.
+# 2. Put at least 5 key-value pairs in your dictionary.
+# 3. Use a for loop to go through the dictionary.
+#        a. Print out a series of statements such as:
+#             Willie is dog
+ # 4. Change one of the values in your dictionary.
+#               a. Print out your dictionary with the change.
+# 5. Add a new key-value pair to your dictionary.
+# a. Print out your dictionary with the new key-value pair added.
+# 6. Remove one of the key-value pairs from your dictionary.
+#          a. Print out your dictionary to show the change.
+# 7. Print out the dictionary’s keys.
+# 8. Print out the dictionary’s values
+
+
+# 22.  Write a python function to create and return a new dictionary from the given dictionary (subject: mark).
+
+# Create a new dictionary with subject having marks more than 50.
+
+# marks = {English: 40,'Maths': 60, 'Hindi: 30,'Chemistry': 46,'Physics': 70}
+
+# mylist={'ENGLISH':40,'MATHS':60,'HINDI':30,'CHEMISTRY':46,'PHYSICS':70}
+
+# for i in range(1):
+#     print('marks','subject')
+
+#     print(mylist)
+# def markdict(orginal_dict):
+#     passed_subject={}
+#     for subject,mark in orginal_dict.items():
+# if mark>50:
+#             passed_subject[subject]=mark
+#     return passedsubject
+# sample_dict={"English":40,"maths":60,"Hindi":30,"Chemistry":46,"Physics":70}
+# passed_subject_dict=ma_rkdict(sample_dict)
+# print(passed_subject_dict
+
+
+# 23.  Write a python function which accepts a sentence and finds the number of letters and digits in the sentence.
+
+# It should return a dictionary in which the key should be letter count and value should be digit count.
+# Ignore the spaces or any other special character in the sentence
+
+# s = input("Input a string")
+# d=l=0
+# for c in s:
+#     if c.isdigit():
+#         d=d+1
+#     elif c.isalpha():
+#         l=l+1
+#     else:
+#         pass
+
+# print("Letters", l)
+# print("Digits", d)
+
+# str_1=input("input a string")
+# d=a=0
+# for i istn r_1:
+#     if i.isdigit():
+#         d=d+1
+#     elif i.isalpha():
+#         a=a+1
+#     else:
+#         pass
+# print("letters",a)
+# print("digital",d)
+
+
+# 24 . Write a Python function which generates and retrnus a dictionary where the keys are numbers between 1 and n (both inclusive) and the values are square of keys.
+
+
+# d=dict()
+# n=43
+# for i in range(1,n+1):
+#     d[i]=i**2
+# print(d)
 
 
 
