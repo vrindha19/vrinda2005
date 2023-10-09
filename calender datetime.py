93a577ceb1f05315c088e6ffa0b500726cc5a46e
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-09 02:00:37 -0400

    Add files via upload

---
+++ calender datetime.py
@@ -0,0 +1,20 @@
+# calender
+
+# import calendar
+
+# yy=2024
+# mm=1
+# import datetime
+# x = datetime. datetime.now()
+# print(x)
+
+# print(calendar.month(yy,mm))
+
+
+# datetime
+
+# import datetime
+# x = datetime. datetime.now()
+# print(x)
+
+
---
+++ math,random modules.py
@@ -0,0 +1,13 @@
+# math
+
+# import math
+# print(math.pi)
+
+# random
+
+# import random
+# list1 = [1,2,3,4,5,6]
+# print(random.choice(list1))
+
+
+
