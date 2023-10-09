72b012a52ea6e814b4025cb17438929dd6ada143
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-09 11:45:13 +0530

    Update calender datetime.py

--- calender datetime.py
+++ calender datetime.py
@@ -1,4 +1,4 @@
-# calender
+# 1.  calender
 
 # import calendar
 
@@ -11,10 +11,18 @@
 # print(calendar.month(yy,mm))
 
 
-# datetime
+# 2  datetime
 
 # import datetime
 # x = datetime. datetime.now()
 # print(x)
 
+#  3 date of birth
 
+# from datetime import date
+# my_date=date(2005,1,23)
+# print("My Date  Of Birth",my_date)
+# import calendar
+# yy=2005
+# mm=1
+# print(calendar.month(yy,mm))
