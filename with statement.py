650c5596a86b12e96870c2926937f86c719eaf85
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-10 03:17:30 -0400

    Add files via upload

---
+++ with statement.py
@@ -0,0 +1,8 @@
+# with statement
+
+# with open("file_path",'w')as file:
+#     file.write("spectrum software solution")
+
+
+# with open("file_path",'a')as file:
+#     file.write("welcome to python")
---
+++ writing.py
@@ -0,0 +1,27 @@
+# writing 
+
+# f = open ("write.txt","w")
+# f.write(" vrinda trainee python full stack")
+# f = open("write.txt", "r")
+# print(f.read()) 
+
+# f=open("writingg.txt","w")
+# f.write("python")
+# f = open("writingg.txt", "r")
+# print(f.read()) 
+
+# append
+
+# f = open("append.txt","a")
+# x = ["python\n","java\n","html "]
+# f.write("subject \n ")
+# f.writelines(x)
+# f.close()
+
+# f = open("vrinda.txt","a")
+# x = ["Vrinda \n","Manoop \n","Midhun \n","Mridhula"]
+# f.write("Names \n")
+# f.writelines(x)
+# f.close()
+
+
