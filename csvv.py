143c5093935f295efb1650066c5a64bb25f0f138
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-11 03:05:47 -0400

    Add files via upload

---
+++ csvv.py
@@ -0,0 +1,30 @@
+
+# import csv
+# with open('mysheet.xls', 'r') as file:
+#     csv_reader = csv.reader(file)
+#     for row in csv_reader:
+#         print(row)
+
+
+# import csv
+# file_path="C:/Users/Python/Documents/__pycache__/hyyy.csv"
+# encoding="utf-8"
+# with open(file_path,"r",encoding=encoding,newline='') as csvfile:
+#         readd = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#         for row in readd:
+#                 print(":".join(row))
+
+# import csv
+# file_path="C:/Users/Python\Documents/__pycache__/helloo.csv"
+# encoding="utf-8"
+# data_to_write = [
+#     ["research", "operations"],
+#     ["value1", "value2"],  # Replace with your actual data
+#     ["value3", "value4"],
+# ]
+# with open(file_path,"w",encoding=encoding,newline='') as csvfile:
+#     csvwriter=csv.writer(csvfile)
+#     csvwriter.writerows(data_to_write) 
+
+
+
---
+++ jsonn.py
@@ -0,0 +1,13 @@
+# json
+
+# import json
+# # Writing JSON data to a file
+# x = [1, "sample", "hello"]
+# with open("C:/Users/Python/Documents/__pycache__/liya.txt", "w") as f:
+#     json.dump(x, f)
+# # Reading JSON data from the file
+# with open("C:/Users/Python/Documents/__pycache__/liya.txt", "r") as f:
+#     data = json.load(f)
+
+# # # Now, 'data' contains the JSON data from the file as a Python list
+# print(data)
