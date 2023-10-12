c83299356bd7a46d008de242c72f31d8cc4e0a0d
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-12 11:20:57 +0530

    Update zipzz.py

--- zipzz.py
+++ zipzz.py
@@ -62,6 +62,25 @@
 
 
 
+# import zipfile
+
+# # Open the ZIP file for reading
+# with zipfile.ZipFile('zippppp.zip', 'r') as zip_file:
+#     # List the contents of the ZIP file
+#     file_list = zip_file.namelist()
+#     print("Contents of the ZIP file:", file_list)
+
+#     # Extract all files from the ZIP archive
+#     for file_name in file_list:
+#         zip_file.extract(file_name, "C:/Users/Python/Pictures")
+
+
+
+
+
+
+
+
 
 
 
