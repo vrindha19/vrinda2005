c820116568404c1a1e11d43a3aa65351b9560d8e
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-10-14 01:41:09 -0400

    Add files via upload

---
+++ Machine Test.py
@@ -0,0 +1,24 @@
+# 1. Python program to check given number is even or odd
+
+
+# x = 24
+   
+# if x % 24== 0:
+#     print(x,"Is Even Number")
+# else:
+#     print(x, "Is Odd Number")
+     
+# y = 19
+   
+# if y % 19 == 0:
+#     print(y,"Is odd Number")
+# else:
+#     print(y, "Is even Number")
+
+
+# 2. Create a base class called animal with a method speak().
+# create two derived classes Dog and cat that inherit from animal and override the speaks()
+# method to print"Woof!"and"Meow!" respectively.Create instances of both both dog and cat and call their speak() methods.
+
+
+
